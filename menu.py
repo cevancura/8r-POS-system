ca6fb8e776baa7e641cbc95749934bb6a31ed9a6
Christiana Vancura <cevancura@tamu.edu>
2023-10-02 18:29:27 -0500

    initial menu.py with Drink class

---
+++ menu.py
@@ -0,0 +1,19 @@
+# Order History
+
+
+
+class Drink:
+    drink_id : str
+    name : str
+    price : float
+
+
+drinks = {
+    "0001" : Drink("0001", "Classic Black Milk Tea", 5),
+    "0002" : Drink("0002", "Classic Green Milk Tea", 5),
+    "0003" : Drink("0003", "Classic Oolong Milk Tea", 5),
+    "0004" : Drink("0004", "Honey Black Milk Tea", 5),
+    "0005" : Drink("0005", "Honey Green Milk Tea", 5),
+    "000"6 : Drink("0006", "Honey Oolong Milk Tea", 5),
+
+}
