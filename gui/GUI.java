4dc7b50dac6b636d0041d3c14ce6c0debcecfba2
navyamittal <navyamittal.1998@gmail.com>
2023-10-11 13:43:42 -0500

    reformatting and organizing code

--- gui/GUI.java
+++ gui/GUI.java
@@ -1,34 +1,10 @@
 import java.sql.*;
 import java.awt.event.*;
 import javax.swing.*;
-/*
-  TODO:
-  1) Change credentials for your own team's database
-  2) Change SQL command to a relevant query that retrieves a small amount of data
-  3) Create a JTextArea object using the queried data
-  4) Add the new object to the JPanel p
-*/
+
 // TO RUN::
 // compile with 'javac *.java'
 // run with 'java -cp ".;postgresql-42.2.8.jar" GUI'
-
-/*
-@param arg1 An SQL command as a string
-@return arg1 SQL command as string
-*/
-// public static String sqlcommand(String arg1) {
-//     return arg1;
-// }
-
-// /*
-// @param result, name  A result set, and a column name as a string
-// @return string result from query
-// */
-// public static String database(ResultSet result, String name) {
-//     return result.getString(name) + " ";
-// }
-
-
 
 public class GUI extends JFrame implements ActionListener {
     static JFrame f;
@@ -42,7 +18,6 @@ public class GUI extends JFrame implements ActionListener {
 
     public static Connection createConnection() {
       Connection conn = null;
-      //TODO STEP 1
       try {
         conn = DriverManager.getConnection(
           "jdbc:postgresql://csce-315-db.engr.tamu.edu/csce315331_08r_db",
@@ -65,19 +40,6 @@ public class GUI extends JFrame implements ActionListener {
     }
     public static void main(String[] args)
     {
-      //Building the connection
-      // Connection conn = null;
-      // //TODO STEP 1
-      // try {
-      //   conn = DriverManager.getConnection(
-      //     "jdbc:postgresql://csce-315-db.engr.tamu.edu/csce315331_08r_db",
-      //     "csce315_971_cevancura",
-      //     "password");
-      // } catch (Exception e) {
-      //   e.printStackTrace();
-      //   System.err.println(e.getClass().getName()+": "+e.getMessage());
-      //   System.exit(0);
-      // }
       Connection conn = createConnection();
       JOptionPane.showMessageDialog(null,"Opened database successfully");
       String name = "";
@@ -85,35 +47,32 @@ public class GUI extends JFrame implements ActionListener {
       f = new JFrame("DB GUI");
       manager_frame = new JFrame("Manager GUI");
       employee_frame = new JFrame("Employee GUI");
-      //test = new JFrame("test");
+
       // create a object
       GUI s = new GUI();
+
       // create a panel
       JPanel p = new JPanel();
+
+      //creating buttons for the main page
       JButton b = new JButton("Close");
       JButton manager = new JButton("Manager");
       JButton employee = new JButton("Employee");
+
       // add actionlistener to button
       b.addActionListener(s);
       manager.addActionListener(s);
       employee.addActionListener(s);
-      //TODO Step 3 
+      
       JTextArea text = new JTextArea(name);
-      //TODO Step 4
       p.add(text);
-      // add button to panel
       p.add(b);
       p.add(manager);
       p.add(employee);
       // add panel to frame
       f.add(p);
-      // set the size of frame
       f.setSize(400, 400);
-
       f.setVisible(true);
-      
-      
-
 
       // manager frame
       manager_frame.setSize(400, 400);
@@ -153,9 +112,7 @@ public class GUI extends JFrame implements ActionListener {
         //create a statement object
         Statement stmt = conn.createStatement();
         //create a SQL statement
-        //TODO Step 2
         String sqlStatement = "SELECT * FROM inventory;";
-        //send statement to DBMS
         ResultSet result = stmt.executeQuery(sqlStatement);
         while (result.next()) {
           inventory_items += result.getString("product_id") + " ";
@@ -166,9 +123,7 @@ public class GUI extends JFrame implements ActionListener {
         }
 
         //getting menu items from drinks dictionary
-
         String menu_command = "SELECT * FROM drink_dictionary;";
-        //send statement to DBMS
         ResultSet menuresult = stmt.executeQuery(menu_command);
         while (menuresult.next()) {
           menu_items += menuresult.getString("drink_id") + " ";
@@ -205,17 +160,10 @@ public class GUI extends JFrame implements ActionListener {
       drinks_frame.add(p_menu);
       drinks_frame.setSize(800, 800);
 
-      
       manager_frame.add(p_man);
 
       //closing the connection
       closeConnection(conn);
-      // try {
-      //   conn.close();
-      //   JOptionPane.showMessageDialog(null,"Connection Closed.");
-      // } catch(Exception e) {
-      //   JOptionPane.showMessageDialog(null,"Connection NOT Closed.");
-      // }
     }
   
 
@@ -255,28 +203,18 @@ public class GUI extends JFrame implements ActionListener {
           if (!text_output.getText().equals("")) {
             System.out.println("Hello?");
             Connection conn = null;
-            //TODO STEP 1
+            
             try {
               conn = DriverManager.getConnection(
                 "jdbc:postgresql://csce-315-db.engr.tamu.edu/csce315331_08r_db",
                 "csce315_971_navya_0215",
                 "password");
             } catch (Exception k) {
-              // e.printStackTrace();
-              // System.err.println(e.getClass().getName()+": "+e.getMessage());
               System.exit(0);
             }
             try{
-            //create a statement object
             
             Statement stmt = conn.createStatement();
-            
-            //create a SQL statement
-            //TODO Step 2
-            //String sqlStatement = sqlcommand("SELECT * FROM inventory;");
-            //send statement to DBMS
-            //ResultSet result = stmt.executeQuery(sqlStatement);
-
             String[] splitted = text_output.getText().split("\\s+");
             
             String menu_update = "INSERT INTO drink_dictionary (drink_id, name, price) VALUES";
@@ -306,10 +244,8 @@ public static String database(ResultSet result, String name) {
     while (result.next()) {
       response += result.getString("name")+"\n";
     }
-    //return response;
 
     }
-    
     catch (Exception e){
         JOptionPane.showMessageDialog(null,"Error accessing Database.");
     }
